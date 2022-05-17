17e05b4bf37be3810c6a048636ccd7198e043d5c
Kvasok-Stepana <102612188+Kvasok-Stepana@users.noreply.github.com>
2022-05-17 19:35:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <utility>
+#include <functional>
+#include <iterator>
+#include <string>
+#include <cstring>
+
+#include "rk2_tasks.h"
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+    /*
+    Filo <int> pr(5);
+
+    for (int i = 0; i < 5; i++) {
+        pr.push_back(i);
+        auto x =pr.pop_back();
+        cout<<x.first<<" "<<x.second;
+        cout<<endl;
+    }
+*/
+
+  /* int arr[10];
+    for (int i = 0; i <9 ; ++i) {
+        arr[i]=i+1;
+        cout<<arr[i];
+    }
+    cout<<endl;
+    for (int i = 0; i < 9; ++i) {
+        cout<<binSearch(arr,9,i+1).first<<endl;
+        cout<<binSearch(arr,9,i+1).second<<endl;
+        cout<<endl;
+    }
+*/
+
+
+    return 0;
+}
